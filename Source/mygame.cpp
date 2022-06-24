a2743703c69098cdf7991c976bd452e6b3155371
Eddie <kta23379745@gmail.com>
2022-06-24 12:00:48 +0800

    fix bomb bug with bomb all brick

--- Source/mygame.cpp
+++ Source/mygame.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ac919b493fd5fdbd05704e6454147b9c5127b3d65ad4683cf5c69b959705f4d
-size 43158
+oid sha256:f687c669f908ee7789209d1d03f0b1ec05d7a800a72839bd16164d2bce8cdc32
+size 43194
